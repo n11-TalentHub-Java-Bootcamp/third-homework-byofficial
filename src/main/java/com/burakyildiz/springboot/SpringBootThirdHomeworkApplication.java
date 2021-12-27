139a896bf340f9220864085478299ab3271f04b2
Burak <burak.yildiz0@outlook.com>
2021-12-27 12:45:30 +0300

    Create spring-boot project

---
+++ src/main/java/com/burakyildiz/springboot/SpringBootThirdHomeworkApplication.java
@@ -0,0 +1,15 @@
+package com.burakyildiz.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringBootThirdHomeworkApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootThirdHomeworkApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/burakyildiz/springboot/SpringBootThirdHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burakyildiz.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThirdHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
